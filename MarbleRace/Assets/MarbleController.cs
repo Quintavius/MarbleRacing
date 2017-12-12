01badfce43e5ffbb082b215b42415e11577aacd5
Quintavius <loths.marc@gmail.com>
2017-12-13 00:25:41 +1300

    feels good to be a marble

--- MarbleRace/Assets/MarbleController.cs
+++ MarbleRace/Assets/MarbleController.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MarbleController : MonoBehaviour {
-    public float movementSpeed;
+    public float acceleration;
     public float maximumSpeed;
+    public float slopeSpeed;
 
     Rigidbody rb;
+    Collider col;
     Camera cam;
-    //Vector3 steerDir;
+    public GameObject stabilizer;
 
     float hmove;
     float vmove;
+
+    public float mag;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
         cam = Camera.main;
+        col = GetComponent<Collider>();
 	}
 	
 	// Update is called once per frame
@@ -24,11 +29,23 @@ public class MarbleController : MonoBehaviour {
         vmove = Input.GetAxis("Vertical");
 
         if (hmove != 0 || vmove != 0) {
-            //Get force direction
-            Vector3 dir = new Vector3(hmove, 0, vmove);
-            Vector3 steerDir = cam.transform.TransformDirection(dir);
-            //LET THE FORCE FLOW THROUGH YOU
-            rb.AddForce(steerDir * movementSpeed * Time.deltaTime);
+            if (rb.velocity.magnitude < maximumSpeed * 1.5f)
+            {
+                //Get force direction
+                Vector3 dir = new Vector3(hmove, 0, vmove);
+                Vector3 steerDir = stabilizer.transform.TransformDirection(dir);
+                //LET THE FORCE FLOW THROUGH YOU
+                rb.AddForce(steerDir * acceleration * Time.deltaTime);
+            }
         }
 	}
+    void OnCollisionStay(Collision colInfo)
+    {
+        mag = rb.velocity.magnitude;
+        if (rb.velocity.y < 0 && mag < maximumSpeed)
+        {
+            rb.AddForce(new Vector3(rb.velocity.x * slopeSpeed, rb.velocity.y * slopeSpeed, rb.velocity.z * slopeSpeed));
+        }
+
+    }
 }
---
+++ MarbleRace/Assets/Stabilizer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stabilizer : MonoBehaviour {
+    Transform cam;
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        transform.eulerAngles = new Vector3(0,cam.eulerAngles.y,0);
+	}
+}
