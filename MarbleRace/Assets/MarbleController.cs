0eaa3083ca5cbee452dcd3d01152b01b397abf30
Quintavius <loths.marc@gmail.com>
2017-12-12 20:28:49 +1300

    Starting

---
+++ MarbleRace/Assets/MarbleController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarbleController : MonoBehaviour {
+    public float movementSpeed;
+    public float maximumSpeed;
+
+    Rigidbody rb;
+    Camera cam;
+    //Vector3 steerDir;
+
+    float hmove;
+    float vmove;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        hmove = Input.GetAxis("Horizontal");
+        vmove = Input.GetAxis("Vertical");
+
+        if (hmove != 0 || vmove != 0) {
+            //Get force direction
+            Vector3 dir = new Vector3(hmove, 0, vmove);
+            Vector3 steerDir = cam.transform.TransformDirection(dir);
+            //LET THE FORCE FLOW THROUGH YOU
+            rb.AddForce(steerDir * movementSpeed * Time.deltaTime);
+        }
+	}
+}
