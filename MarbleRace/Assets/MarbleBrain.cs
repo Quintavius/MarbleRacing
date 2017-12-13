1d5fabb0bc750ca83cbb1620402ad5d0a1908040
Quintavius <loths.marc@gmail.com>
2017-12-13 20:04:26 +1300

    bout to update, more ai

--- MarbleRace/Assets/MarbleBrain.cs
+++ MarbleRace/Assets/MarbleBrain.cs
@@ -8,6 +8,8 @@ public class MarbleBrain : MonoBehaviour {
     public float maximumSpeed;
     public float slopeSpeed;
 
+    public Transform[] targets;
+    int targetIndex;
     //Shorthands
     [HideInInspector]
     public Rigidbody rb;
@@ -23,13 +25,42 @@ public class MarbleBrain : MonoBehaviour {
     int colCount;
 
     void Start () {
-		
-	}
+        //initialize
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+        //soundManager = GetComponent<MarbleSoundManager>();
+    }
 	
-	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (rb.velocity.magnitude < maximumSpeed * 1.5f)
+        {
+            //Get target direction
+            Vector3 dir = targets[0].position - transform.position;
+            dir = new Vector3(dir.x, 0, dir.z);
+            //LET THE FORCE FLOW THROUGH YOU
+            rb.AddForce(dir * acceleration * Time.deltaTime);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (colCount == 0) { grounded = false; } else { grounded = true; }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        colCount++;
+    }
+
+        void OnCollisionStay(Collision colInfo)
+    {
+        //Increase speed when rolling
+        mag = rb.velocity.magnitude;
+        if (rb.velocity.y < 0 && mag < maximumSpeed)
+        {
+            rb.AddForce(new Vector3(rb.velocity.x * slopeSpeed, rb.velocity.y * slopeSpeed, rb.velocity.z * slopeSpeed));
+        }
+    }
 
     private void OnCollisionExit(Collision collision)
     {
