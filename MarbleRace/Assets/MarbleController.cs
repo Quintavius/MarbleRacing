3d508e0ed1772566df8dbffee75d08c328ec6bec
Quintavius <loths.marc@gmail.com>
2017-12-13 17:39:18 +1300

    marble sounds

--- MarbleRace/Assets/MarbleController.cs
+++ MarbleRace/Assets/MarbleController.cs
@@ -3,31 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MarbleController : MonoBehaviour {
+    [Header("Player Settings")]
+    public int playerId;
+    public bool AIMarble;
+
+    [Header("Movement Settings")]
     public float acceleration;
     public float maximumSpeed;
     public float slopeSpeed;
 
-    Rigidbody rb;
+    //Shorthands
+    [HideInInspector]
+    public Rigidbody rb;
     Collider col;
     Camera cam;
     public GameObject stabilizer;
+    MarbleSoundManager soundManager;
 
+    //Variables
     float hmove;
     float vmove;
-
+    string axisH;
+    string axisV;
+    [HideInInspector]
     public float mag;
-	// Use this for initialization
+    [HideInInspector]
+    public bool grounded;
+    public bool playDropSound;
+    int colCount;
+
 	void Start () {
+        //initialize
         rb = GetComponent<Rigidbody>();
         cam = Camera.main;
         col = GetComponent<Collider>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        hmove = Input.GetAxis("Horizontal");
-        vmove = Input.GetAxis("Vertical");
+        soundManager = GetComponent<MarbleSoundManager>();
 
+        //Assign player ID
+        if (playerId == 0) { axisH = "Horizontal_Player1"; axisV = "Vertical_Player1"; }
+        else if (playerId == 1) { axisH = "Horizontal_Player2"; axisV = "Vertical_Player2"; }
+    }
+	
+	void Update () {
+        hmove = Input.GetAxis(axisH);
+        vmove = Input.GetAxis(axisV);
+
+        //Check for input
         if (hmove != 0 || vmove != 0) {
             if (rb.velocity.magnitude < maximumSpeed * 1.5f)
             {
@@ -39,13 +60,61 @@ public class MarbleController : MonoBehaviour {
             }
         }
 	}
+
+    private void FixedUpdate()
+    {
+        if (colCount == 0) { grounded = false; } else { grounded = true; }
+    }
+
     void OnCollisionStay(Collision colInfo)
     {
+        //Increase speed when rolling
         mag = rb.velocity.magnitude;
         if (rb.velocity.y < 0 && mag < maximumSpeed)
         {
             rb.AddForce(new Vector3(rb.velocity.x * slopeSpeed, rb.velocity.y * slopeSpeed, rb.velocity.z * slopeSpeed));
         }
+    }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        colCount++;
+
+        if (collision.gameObject.tag == "Player")
+        {
+            //We've hit a player, do a click noise
+            var impact = Mathf.Clamp(collision.impulse.sqrMagnitude + 0.1f,0,1.5f);
+            soundManager.MarbleSound(impact);
+
+            Rigidbody rbOther = collision.gameObject.GetComponent<Rigidbody>();
+            rbOther.AddForceAtPosition(collision.impulse * 10, collision.contacts[0].point);
+            rb.AddForceAtPosition(collision.impulse * 10, collision.contacts[0].point);
+        }
+        else
+        {
+            //are we touching multiple objects and did we hit hard enough to make a sound
+            if (collision.impulse.magnitude > 0.01f && colCount > 1f)
+            {
+                var impact = collision.impulse.magnitude / 3;
+                soundManager.WallSound(impact);
+                
+            }
+            else
+            {
+                //Did we just fall?
+                if (!grounded) { playDropSound = true; }
+                if (playDropSound)
+                {
+                    var impact = collision.impulse.magnitude/3;
+                    soundManager.DropSound(impact);
+                    playDropSound = false;
+                }
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        colCount--;
     }
 }
---
+++ MarbleRace/Assets/MarbleSoundManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MarbleSoundManager : MonoBehaviour {
+    public AudioMixer playerMixer;
+    MarbleController marble;
+
+    AudioSource rollSound;
+    AudioSource startSound;
+    AudioSource dropSound;
+    AudioSource clickSound;
+
+    public AudioClip[] dropSounds;
+
+    //Variables
+    bool stopped;
+
+	void Start () {
+        marble = GetComponent<MarbleController>();
+
+        AudioSource[] audios = GetComponents<AudioSource>();
+        rollSound = audios[0];
+        startSound = audios[1];
+        dropSound = audios[2];
+        clickSound = audios[3];
+	}
+	
+	void Update () {
+        var rollVolumeMod = marble.mag / (marble.maximumSpeed * 1.5f);
+        //Rolling SFX
+        if (marble.grounded)
+        {
+            rollSound.volume = rollVolumeMod;
+            rollSound.pitch = Mathf.Lerp(0, 1f, rollVolumeMod);
+        }
+        else
+        {
+            rollSound.volume = 0;
+        }
+
+        //Starting SFX
+        startSound.volume = Mathf.Clamp01(rollVolumeMod + 0.2f);
+        startSound.pitch = Mathf.Lerp(0, 1f, rollVolumeMod);
+        if (marble.mag == 0)
+        {
+            stopped = true;
+        }
+        else
+        {
+            if (stopped)
+            {
+                startSound.Play();
+                stopped = false;
+            }
+        }
+	}
+
+    public void DropSound(float impactStrength)
+    {
+        var rollVolumeMod = marble.mag / (marble.maximumSpeed * 1.5f);
+        dropSound.volume = impactStrength;
+        dropSound.pitch = Random.Range(0.8f, 1.2f);
+        int dropArraySize = dropSounds.Length;
+        AudioClip chosenDropSound = dropSounds[Random.Range(0, dropArraySize)];
+        dropSound.PlayOneShot(chosenDropSound);
+    }
+
+    public void MarbleSound(float impactStrength)
+    {
+        clickSound.volume = impactStrength;
+        clickSound.pitch = Random.Range(0.5f, 1.5f);
+        clickSound.Play();
+    }
+
+    public void WallSound(float impactStrength)
+    {
+        var rollVolumeMod = marble.mag / (marble.maximumSpeed * 1.5f);
+        dropSound.volume = impactStrength;
+        dropSound.pitch = Random.Range(0.8f, 1.2f);
+        int dropArraySize = dropSounds.Length;
+        AudioClip chosenDropSound = dropSounds[Random.Range(0, dropArraySize)];
+        dropSound.PlayOneShot(chosenDropSound);
+    }
+}
