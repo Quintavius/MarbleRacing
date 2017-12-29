482203ce130849b8f67dde458532e4767dbde14a
Quintavius <loths.marc@gmail.com>
2017-12-29 15:19:34 +1300

    mothafucking $$$ loot box GAINS

---
+++ MarbleRace/Assets/EggController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class EggController : MonoBehaviour {
+    public float blastForce;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.name == "Player")
+        {
+            foreach (Transform child in transform)
+            {
+                child.GetComponent<Rigidbody>().isKinematic = false;
+                child.GetComponent<Rigidbody>().AddExplosionForce(blastForce, other.transform.position, 5);
+            }
+            GetComponent<AudioSource>().Play();
+            Destroy(transform.GetComponent<Collider>());
+        }
+    }
+}
