db58ba53f550dd1891fa2a806af110ed2000083b
Quintavius <loths.marc@gmail.com>
2017-12-30 11:46:35 +1300

    4 Cameras following players

---
+++ MarbleRace/Assets/PlayerFollowCam.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollowCam : MonoBehaviour {
+	public Transform followPlayer;
+	Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - followPlayer.transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = followPlayer.transform.position + offset;
+	}
+}
