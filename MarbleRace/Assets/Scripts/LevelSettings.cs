988d0ddb10db7860bbb58fa2d8313c62f66ab908
Quintavius <loths.marc@gmail.com>
2017-12-31 17:06:37 +1300

    Started on LevelSettings

---
+++ MarbleRace/Assets/Scripts/LevelSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class LevelSettings {
+	static int numberOfPlayers;
+	static string player1Name;
+	static string player2Name;
+	static string player3Name;
+	static string player4Name;
+	static Marble.Skin player1Skin;
+	static Marble.Skin player2Skin;
+	static Marble.Skin player3Skin;
+	static Marble.Skin player4Skin;
+}
