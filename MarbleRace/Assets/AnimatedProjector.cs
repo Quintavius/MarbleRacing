d3b46a8343d64099ebf573d7e7703cf59c1ae192
Quintavius <loths.marc@gmail.com>
2018-01-31 09:30:29 +1300

    Added water section

---
+++ MarbleRace/Assets/AnimatedProjector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedProjector : MonoBehaviour {
+	public float fps = 60f;
+	public Texture2D[] frames;
+	private int frameIndex;
+	private Projector projector;
+	// Use this for initialization
+	void Start () {
+		projector = GetComponent<Projector>();
+		InvokeRepeating("NextFrame", 0, 3/fps);
+	}
+
+	void NextFrame() {
+		projector.material.SetTexture("_MainTex", frames[frameIndex]);
+		frameIndex = (frameIndex + 1) % frames.Length;
+	}
+}
