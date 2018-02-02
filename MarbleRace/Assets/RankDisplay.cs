62569b0a87f18bc113b740d061f665f89f0638a5
Quintavius <loths.marc@gmail.com>
2018-02-03 12:42:08 +1300

    Trying showing rank on marble

---
+++ MarbleRace/Assets/RankDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RankDisplay : MonoBehaviour {
+	public Transform playerToFollow;
+	MarbleRank playerRank;
+	Text rankText;
+	void Start(){
+		rankText = GetComponentInChildren<Text>();
+		playerRank = playerToFollow.GetComponent<MarbleRank>();
+	}
+	void Update(){
+		rankText.text = playerRank.currentRank.ToString();
+	}
+	void FixedUpdate () {
+		transform.position = playerToFollow.position;
+		transform.LookAt(Camera.main.transform.position);
+	}
+}
